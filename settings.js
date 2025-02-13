a2c488c8275e2b3cbb40f97fad839e703f269ae9
Aftablaghari <laghariaftab656@gmail.com>
2025-02-14 01:45:06 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'whyaftab',
+  author: 'aftab',
+  botName: "whycrazy",
+  botOwner: 'whyaftab', // Your name
+  ownerNumber: '923103278551', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
